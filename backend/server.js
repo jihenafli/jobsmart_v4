7a512d9c54abd943ebaabffc9d39606bbc367f05
Jihene Afli <aflijihen107@gmail.com>
2026-04-29 23:16:30 +0200

    add cors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,14 @@ const rateLimit = require('express-rate-limit');
 const app = express();
 app.set('trust proxy', 1);
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000', credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://jobsmart-v4.vercel.app"
+  ],
+  credentials: true
+}));
+// app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000', credentials: true }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
